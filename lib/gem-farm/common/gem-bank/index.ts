1ab26feeae6eb7058a9d9a2201f3cf33a85e75c2
Shaik-Sirajuddin <sirajuddinshaik30@gmail.com>
2022-05-27 08:08:21 +0530

    final commit

--- lib/gem-farm/common/gem-bank/index.ts
+++ lib/gem-farm/common/gem-bank/index.ts
@@ -1,13 +1,13 @@
-import { BN, Idl } from "@project-serum/anchor"
+import { BN, Idl } from '@project-serum/anchor';
 import {
   findWhitelistProofPDA,
   GEM_BANK_PROG_ID,
   GemBankClient,
-  WhitelistType
-} from "@gemworks/gem-farm-ts"
-import { Connection, Keypair, PublicKey } from "@solana/web3.js"
-import { SignerWalletAdapter } from "@solana/wallet-adapter-base"
-import { NodeWallet, programs } from "@metaplex/js"
+  WhitelistType,
+} from '@gemworks/gem-farm-ts';
+import { Connection, Keypair, PublicKey } from '@solana/web3.js';
+import { SignerWalletAdapter } from '@solana/wallet-adapter-base';
+import { NodeWallet, programs } from '@metaplex/js';
 
 //when we only want to view vaults, no need to connect a real wallet.
 export function createFakeWallet() {
@@ -16,10 +16,10 @@ export function createFakeWallet() {
       208, 175, 150, 242, 88, 34, 108, 88, 177, 16, 168, 75, 115, 181, 199, 242,
       120, 4, 78, 75, 19, 227, 13, 215, 184, 108, 226, 53, 111, 149, 179, 84,
       137, 121, 79, 1, 160, 223, 124, 241, 202, 203, 220, 237, 50, 242, 57, 158,
-      226, 207, 203, 188, 43, 28, 70, 110, 214, 234, 251, 15, 249, 157, 62, 80
+      226, 207, 203, 188, 43, 28, 70, 110, 214, 234, 251, 15, 249, 157, 62, 80,
     ])
-  )
-  return new NodeWallet(leakedKp)
+  );
+  return new NodeWallet(leakedKp);
 }
 
 //need a separate func coz fetching IDL is async and can't be done in constructor
@@ -27,24 +27,24 @@ export async function initGemBank(
   conn: Connection,
   wallet?: SignerWalletAdapter
 ) {
-  const walletToUse = wallet ?? createFakeWallet()
-  const idl = await (await fetch("gem_bank.json")).json()
-  return new GemBank(conn, walletToUse as any, idl)
+  const walletToUse = wallet ?? createFakeWallet();
+  const idl = await (await fetch('gem_bank.json')).json();
+  return new GemBank(conn, walletToUse as any, idl);
 }
 
 export class GemBank extends GemBankClient {
   constructor(conn: Connection, wallet: any, idl: Idl) {
-    super(conn, wallet, idl, GEM_BANK_PROG_ID)
+    super(conn, wallet, idl, GEM_BANK_PROG_ID);
   }
 
   async initBankWallet() {
-    const bank = Keypair.generate()
+    const bank = Keypair.generate();
     const txSig = await this.initBank(
       bank,
       this.wallet.publicKey,
       this.wallet.publicKey
-    )
-    return { bank, txSig }
+    );
+    return { bank, txSig };
   }
 
   async initVaultWallet(bank: PublicKey) {
@@ -53,8 +53,8 @@ export class GemBank extends GemBankClient {
       this.wallet.publicKey,
       this.wallet.publicKey,
       this.wallet.publicKey,
-      "test_vault"
-    )
+      'test_vault'
+    );
   }
 
   async setVaultLockWallet(
@@ -62,7 +62,7 @@ export class GemBank extends GemBankClient {
     vault: PublicKey,
     vaultLocked: boolean
   ) {
-    return this.setVaultLock(bank, vault, this.wallet.publicKey, vaultLocked)
+    return this.setVaultLock(bank, vault, this.wallet.publicKey, vaultLocked);
   }
 
   async depositGemWallet(
@@ -73,9 +73,9 @@ export class GemBank extends GemBankClient {
     gemSource: PublicKey,
     creator: PublicKey
   ) {
-    const [mintProof, bump] = await findWhitelistProofPDA(bank, gemMint)
-    const [creatorProof, bump2] = await findWhitelistProofPDA(bank, creator)
-    const metadata = await programs.metadata.Metadata.getPDA(gemMint)
+    const [mintProof, bump] = await findWhitelistProofPDA(bank, gemMint);
+    const [creatorProof, bump2] = await findWhitelistProofPDA(bank, creator);
+    const metadata = await programs.metadata.Metadata.getPDA(gemMint);
 
     return this.depositGem(
       bank,
@@ -87,7 +87,7 @@ export class GemBank extends GemBankClient {
       mintProof,
       metadata,
       creatorProof
-    )
+    );
   }
 
   async withdrawGemWallet(
@@ -103,7 +103,7 @@ export class GemBank extends GemBankClient {
       gemAmount,
       gemMint,
       this.wallet.publicKey
-    )
+    );
   }
 
   async addToWhitelistWallet(
@@ -116,7 +116,7 @@ export class GemBank extends GemBankClient {
       this.wallet.publicKey,
       addressToWhitelist,
       whitelistType
-    )
+    );
   }
 
   async removeFromWhitelistWallet(bank: PublicKey, addressToRemove: PublicKey) {
@@ -124,6 +124,6 @@ export class GemBank extends GemBankClient {
       bank,
       this.wallet.publicKey,
       addressToRemove
-    )
+    );
   }
 }
